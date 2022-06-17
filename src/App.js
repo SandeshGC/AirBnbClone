a6ba9ff3b60b55ecd49b3dafe6c649999b00baef
Sandesh GC <gcsandesh01@gmail.com>
2022-06-17 09:31:01 +0545

    finalized the AirBnb Clone page

--- src/App.js
+++ src/App.js
@@ -3,20 +3,14 @@ import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Card from "./components/Card";
 import data from "./data";
-// import "./images/katie-zaferes.png"
 function App() {
-  const arrayOfCards = data.map((datum) => { //an array of card components is formed by mapping data array
-    return (  //mapping data array to card components with props taken from the data array
-      <Card
-        coverImg={datum.coverImg}
-        title={datum.title}
-        price={datum.price}
-        stats={datum.stats}
-        location={datum.location}
-      />
+  const arrayOfCards = data.map((datum) => {
+    //an array of card components is formed by mapping data array
+    return (
+      //mapping data array to card components with props taken from the data array
+      <Card key={datum.id} {...datum} />
     );
   });
-  console.log(data);
   return (
     <>
       <NavBar />
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,16 +1,34 @@
 import React from "react";
 export default function Card(props) {
+  let badgeText;
+  if (props.openSpots === 0) {
+    badgeText = "SOLD OUT";
+  } else if (props.location === "Online") {
+    badgeText = "ONLINE";
+  }
   return (
     <div className="card">
       <img
         className="card--cover-image"
         src={require(`../images/${props.coverImg}`)}
-      />
+      />{" "}
+      {/*cover image of each card*/}
+      {badgeText && (
+        <div className="card--badge" id="badge">
+          {badgeText}
+        </div>
+      )}{" "}
+      {/*badge on card for online or sold out*/}
       <div className="card--details">
-        <img className="card--star-icon" src={require("../images/star.png")} />
+        <img
+          className="card--star-icon"
+          src={require("../images/star.png")}
+          alt="star icon"
+        />{" "}
+        {/*star icon of every card rating*/}
         {props.stats.rating}
         <span className="grey-text">
-           ({props.stats.reviewCount}) • {props.location}
+          ({props.stats.reviewCount}) • {props.location}
         </span>
       </div>
       <p className="card--title">{props.title}</p>
